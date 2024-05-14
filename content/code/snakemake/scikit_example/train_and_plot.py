8a06b2a99c067c7d524da047787570f110a140a3
Teemu Ruokolainen <teemu.p.ruokolainen@gmail.com>
2024-05-14 12:56:00 +0300

    FIX: copy correct version of train_and_plot.py to snakemake/scikit_example/

--- content/code/snakemake/scikit_example/train_and_plot.py
+++ content/code/snakemake/scikit_example/train_and_plot.py
@@ -22,9 +22,14 @@ parser.add_argument(
     type=int,
     help="The number of neighbors to use for calculation.",
 )
-
+parser.add_argument(
+    "--metric",
+    type=str,
+    help="The metric to use",
+)
 args = parser.parse_args()
 n_neighbors = args.n_neighbors
+metric = args.metric
 
 # Load preprocessed data from disk
 with open("data/preprocessed/Iris.pkl", "rb") as f:
@@ -34,43 +39,40 @@ with open("data/preprocessed/Iris.pkl", "rb") as f:
 
 # Parameters
 # Metrics: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.distance_metrics.html#sklearn.metrics.pairwise.distance_metrics
-metrics = ["euclidean", "manhattan", "l1", "haversine", "cosine"]
 
-# Loop over metrics
-for metric in metrics:
-    # Fit
-    clf = Pipeline(
-        steps=[
-            ("scaler", StandardScaler()),
-            ("knn", KNeighborsClassifier(n_neighbors=n_neighbors, metric=metric)),
-        ]
-    )
-    clf.fit(X_train, y_train)
+# Loop over n_neighbors
+# Fit
+clf = Pipeline(
+    steps=[
+        ("scaler", StandardScaler()),
+        ("knn", KNeighborsClassifier(n_neighbors=n_neighbors, metric=metric)),
+    ]
+)
+clf.fit(X_train, y_train)
 
-    # Plot
-    disp = DecisionBoundaryDisplay.from_estimator(
-        clf,
-        X_test,
-        response_method="predict",
-        plot_method="pcolormesh",
-        xlabel=features[0],
-        ylabel=features[1],
-        shading="auto",
-        alpha=0.5,
-    )
-    scatter = disp.ax_.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, edgecolors="k")
-    disp.ax_.legend(
-        scatter.legend_elements()[0],
-        classes,
-        loc="lower left",
-        title="Classes",
-    )
-    _ = disp.ax_.set_title(
-        f"3-Class classification\n(k={n_neighbors!r}, metric={metric!r})"
-    )
-    plt.show()
+# Plot
+disp = DecisionBoundaryDisplay.from_estimator(
+    clf,
+    X_test,
+    response_method="predict",
+    plot_method="pcolormesh",
+    xlabel=features[0],
+    ylabel=features[1],
+    shading="auto",
+    alpha=0.5,
+)
+scatter = disp.ax_.scatter(X.iloc[:, 0], X.iloc[:, 1], c=y, edgecolors="k")
+disp.ax_.legend(
+    scatter.legend_elements()[0],
+    classes,
+    loc="lower left",
+    title="Classes",
+)
+_ = disp.ax_.set_title(
+    f"3-Class classification\n(k={n_neighbors!r}, metric={metric!r})"
+)
+plt.show()
 
-    # Save image to disk
-    Path("results/").mkdir(parents=True, exist_ok=True)
-    plt.savefig(f"results/n_neighbors={n_neighbors}___metric={metric}.png")
-    plt.close()
+# Save image to disk
+Path("results/").mkdir(parents=True, exist_ok=True)
+plt.savefig(f"results/n_neighbors={n_neighbors}___metric={metric}.png")
